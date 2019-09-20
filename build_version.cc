b296ddb388ff23d326ecd0f2800fe7090318dcb2
wonki <cwk1412@yonsei.ac.kr>
2019-09-20 16:04:37 +0900

    add torch to export set

--- build_version.cc
+++ build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@bfe7dacae41d2febcb722f445f9147af063f425f@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2019/09/18 01:11:45@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@3c33625c089d6384f798fd56c1082b5cb9f31f82@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2019/09/20 06:58:09@";
 const char* rocksdb_build_compile_date = __DATE__;
